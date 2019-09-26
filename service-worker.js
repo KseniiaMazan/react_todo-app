d1ae313842ab65a52e7fcb4466599aae69ebb391
KseniiaMazan <ksusamazan@gmail.com>
2019-09-26 10:34:26 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_todo-app/precache-manifest.36c0f0e73e4ba58e6c71741bdc35771c.js"
+  "/react_todo-app/precache-manifest.f613219cfabed7171cf34a39988744af.js"
 );
 
 self.addEventListener('message', (event) => {
